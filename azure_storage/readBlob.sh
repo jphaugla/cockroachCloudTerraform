ef95ee2c2d679b0987c18236b711525bd4fe1906
jphaugla <jasonhaugland@gmail.com>
2025-09-25 16:06:04 -0500

    add azure from pre-created azure cluster.  Add more api calls.  Add azure private endpoint and azure storage scripts

--- azure_storage/readBlob.sh
+++ azure_storage/readBlob.sh
@@ -1,8 +1,8 @@
 # set creds once (raw account key, not URL-encoded)
 export AZURE_STORAGE_ACCOUNT=jphauglblahbsa2
-export AZURE_STORAGE_KEY='dckJblah+gRsNblahshiETrzxGkbLxGO9WmWIgzz2ncFM2blahblahzblahS1Yb9g=='
+export AZURE_STORAGE_KEY='blahblah+gRsNblahshiETrzxGkbLxGO9WmWIgzz2ncFM2blahblahzblahS1Yb9g=='
 
-BLOB='cdc/2025-09-25/202509251848430057739660000000000-ccaf287129fbf78c-1-1-00000000-transaction-1.ndjson'
+BLOB='cdc/2025-09-25/blahblah1848430blahblah0000blahblahcaf287129fbf78c-1-1-00000000-transaction-1.ndjson'
 TMP="$(mktemp)"
 
 az storage blob download \
