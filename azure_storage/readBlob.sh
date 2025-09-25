f9a12395b657931653b01a197ec9e460ad53ad24
jphaugla <jasonhaugland@gmail.com>
2025-09-25 16:08:18 -0500

    add azure from pre-created azure cluster.  Add more api calls.  Add azure private endpoint and azure storage scripts

--- azure_storage/readBlob.sh
+++ azure_storage/readBlob.sh
@@ -1,6 +1,7 @@
 # set creds once (raw account key, not URL-encoded)
 export AZURE_STORAGE_ACCOUNT=jphauglblahbsa2
-export AZURE_STORAGE_KEY=''
+#   even having AZURE_STORAGE_KEY is causing issues in the git push
+export AZxxURE_STxxAGE_KEY=''
 
 BLOB='cdc/2025-09-25/blahblah1848430blahblah0000blahblahcaf287129fbf78c-1-1-00000000-transaction-1.ndjson'
 TMP="$(mktemp)"
